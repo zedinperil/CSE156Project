f8d9c8f9f594bedbc3e8d005b1e8f8fdbde4edde
achmelka <chmelkaalex@gmail.com>
2017-02-23 16:50:10 -0600

    Updated Portfolio

--- src/dataConverter/Portfolio.java
+++ src/dataConverter/Portfolio.java
@@ -1,5 +1,68 @@
 package dataConverter;
 
 public class Portfolio {
+	private String PortfolioCode;
+	private String OwnerCode;
+	private String ManagerCode;
+	private String BeneficiaryCode;
+	private String[] Assets;
+	
+	public Portfolio(String portCode, String ownCode, String managCode,
+					 String beneficiaryCode, String[] assets, int[] UsefulNumber, int assetsize) {
+		super();
+		String delimeter = ":";
+		this.PortfolioCode = portCode;
+		this.OwnerCode = ownCode;
+		this.ManagerCode = managCode;
+		this.BeneficiaryCode = beneficiaryCode;
+		int i;
+		for(i=0;i< assetsize; i++){
+		this.Assets[i] = assets[i];
+		}
+	}
 
-}
+	public String getPortfolioCode() {
+		return PortfolioCode;
+	}
+
+	public void setPortfolioCode(String portfolioCode) {
+		PortfolioCode = portfolioCode;
+	}
+
+	public String getOwnerCode() {
+		return OwnerCode;
+	}
+
+	public void setOwnerCode(String ownerCode) {
+		OwnerCode = ownerCode;
+	}
+
+	public String getManagerCode() {
+		return ManagerCode;
+		
+	}
+
+	public void setManagerCode(String managerCode) {
+		ManagerCode = managerCode;
+	}
+
+	public String getBeneficiaryCode() {
+		return BeneficiaryCode;
+	}
+
+	public void setBeneficiaryCode(String beneficiaryCode) {
+		BeneficiaryCode = beneficiaryCode;
+	}
+
+	public String[] getAssets() {
+		return Assets;
+	}
+
+	public void setAssets(String[] assets) {
+		Assets = assets;
+	}
+
+	
+
+	
+}	
