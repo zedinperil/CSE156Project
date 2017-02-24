35f053bc37cd758b31195414fe16fdddd57c4a90
achmelka <chmelkaalex@gmail.com>
2017-02-23 19:24:57 -0600

    WIP SUPER ULTRA PORTFOLIO

--- src/dataConverter/Portfolio.java
+++ src/dataConverter/Portfolio.java
@@ -9,8 +9,20 @@ public class Portfolio {
 	private Assets[][] Ass;
 	private Persons[][] Per;
 	
+	private double fees;
+	private double commisions;
+	
+	private double Risk;
+	private double Value;
+	private double AnnualReturn;
+	private double ReturnRate;
+	
+	private double AggRisk;
+	private int personcount;
+	private int assetcount;
+	
 	public Portfolio(String portCode, String ownCode, String managCode, String beneficiaryCode, 
-					 String assetsA, Persons[][] persons, Assets[][] asset) {
+					 String assetsA, Persons[][] persons, Assets[][] asset, int PersonsCount, int AssetsCount) {
 		super();
 		
 		this.PortfolioCode = portCode;
@@ -20,53 +32,88 @@ public class Portfolio {
 		this.Asset = assetsA;
 		this.Ass = asset;
 		this.Per = persons;
+		this.personcount = PersonsCount;
+		this.assetcount = AssetsCount;
 		
 
 		
 	}
-
 
 	public String getPortfolioCode() {
 		return PortfolioCode;
-	}
-
-	public void setPortfolioCode(String portfolioCode) {
-		PortfolioCode = portfolioCode;
 	}
 
 	public String getOwnerCode() {
 		return OwnerCode;
 	}
 
-	public void setOwnerCode(String ownerCode) {
-		OwnerCode = ownerCode;
-	}
-
 	public String getManagerCode() {
 		return ManagerCode;
-		
-	}
-
-	public void setManagerCode(String managerCode) {
-		ManagerCode = managerCode;
 	}
 
 	public String getBeneficiaryCode() {
 		return BeneficiaryCode;
 	}
 
-	public void setBeneficiaryCode(String beneficiaryCode) {
-		BeneficiaryCode = beneficiaryCode;
-	}
-
-	public String getAssets() {
+	public String getAsset() {
 		return Asset;
 	}
 
-	public void setAssets(String assets) {
-		Asset = assets;
+	public Assets[][] getAss() {
+		return Ass;
 	}
 
+	public Persons[][] getPer() {
+		return Per;
+	}
+
+	public double getFees() {
+		return fees;
+	}
+
+	public double getCommisions() {
+		return commisions;
+	}
+
+	public double getRisk() {
+		for(int i=0; i<2; i++){
+			for(int k=0; k<=assetcount; k++){
+				if(Ass[i][k].getType().contains("P")){
+					if(Ass[i][k].getCode().equals())
+					Riskp= Ass[i][k].getOmegaMeasure()+ exp(e,(-100000/(Ass[i][k].getTotalValue()*PercentOwned)));
+				}
+			}
+		}
+		
+		return Risk;
+	}
+
+	public double getValue() {
+		return Value;
+	}
+
+	public double getAnnualReturn() {
+		return AnnualReturn;
+	}
+
+	public double getReturnRate() {
+		return ReturnRate;
+	}
+
+	public double getAggRisk() {
+		return AggRisk;
+	}
+
+	public int getPersoncount() {
+		return personcount;
+	}
+
+	public int getAssetcount() {
+		return assetcount;
+	}
+
+
+	
 	
 
 	
