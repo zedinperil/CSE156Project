ab8e20050a47275ac7f7c429795363768bb20c6b
zedinperil <zedinperil@gmail.com>
2017-03-31 20:23:27 -0500

    boop

--- src/dataConverter/InputOutput.java
+++ src/dataConverter/InputOutput.java
@@ -1,21 +1,9 @@
 package dataConverter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintStream;
-import java.io.PrintWriter;
 import java.text.DecimalFormat;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.ArrayList;
 import java.util.List;
-//The plan
-//IMPLEMENT JDBC, CONVERT TO LISTS. ESSENTIALLY THATS WHAT WE NEED TO DO.
 public class InputOutput implements databaseinfoandmethods{
-	
-	private static final double[] AnnualReturnSum = null;
-
 	public static void main(String[] args) throws IOException {			
 		//formatting for doubles
 		DecimalFormat DoubleFormat = new DecimalFormat();
@@ -25,17 +13,11 @@ public class InputOutput implements databaseinfoandmethods{
 		DecimalFormat RiskFormat = new DecimalFormat();
 		RiskFormat.setMaximumFractionDigits(4);
 		RiskFormat.setMinimumFractionDigits(4);
-		
-
 		List<Asset> AssetList= new ArrayList<Asset>();
 		List<Portfolio> PortfolioList= new ArrayList<Portfolio>();
-
 		//revamp this to use list increments using the database information.
-	
-		
 		PortfolioList= databaseinfoandmethods.getPortfolios();
 		AssetList= databaseinfoandmethods.getAssets();
-
 		System.out.println("##########################################################################################################################");
 		System.out.println();
 		System.out.println("A Collection of "+PortfolioList.size()+" Portfolios");
@@ -48,7 +30,6 @@ public class InputOutput implements databaseinfoandmethods{
 		double CommissionsSum = 0;
 		double AnnualReturnSum=0;
 		double[] PortfolioAnnualReturnSum= new double[PortfolioList.size()];
-
 		while( g<PortfolioList.size()){
 			System.out.println("PORTFOLIO #"+(g+1)+"/"+PortfolioList.size());
 			System.out.println();
@@ -67,8 +48,6 @@ public class InputOutput implements databaseinfoandmethods{
 					NumOfAssetInPortfolio++;
 				}
 			}
-			
-			
 			System.out.println("# Of Asset within the Portfolio :"+NumOfAssetInPortfolio);
 			System.out.println();
 			System.out.println("FINANCIAL INFORMATION");
@@ -78,12 +57,10 @@ public class InputOutput implements databaseinfoandmethods{
 			System.out.println("---------------------------------------------------------------------------------------------------------------------");
 			System.out.println();
 			int t=0;
-		//need to get: Asset Code, Asset Name, Asset Type, Asset Value Modifier, Annual return, risk, value, and return rate for each asset for the given portfolio Also calculate the stuff you need to
 			int a=0;
 			while(t<AssetList.size()){
 						if(AssetList.get(t).getPortfolioCode().equals(PortfolioList.get(g).getPortfolioCode())){
 							System.out.println("Asset #"+(a+1)+"/"+AssetList.size());
-
 							System.out.println("Asset Code :"+AssetList.get(t).getAssetCode());
 							System.out.println("Asset Name :"+ AssetList.get(t).getAssetName());
 							System.out.println("Asset Type :"+ AssetList.get(t).getAssetType());
@@ -121,14 +98,12 @@ public class InputOutput implements databaseinfoandmethods{
 		System.out.println();
 		System.out.println("SUMS OF PORTFOLIO VALUES");
 		System.out.println();
-		System.out.println("Sum of All Portfolio Total Asset Values :$"+DoubleFormat.format(TotalValueSum));
-		
+		System.out.println("Sum of All Portfolio Total Asset Values :$"+DoubleFormat.format(TotalValueSum));		
 		System.out.println("Sum of All Portfolio Annual Returns :$"+DoubleFormat.format(AnnualReturnSum));
 		System.out.println("Sum of All Portfolio Commissions :$"+DoubleFormat.format(CommissionsSum));
 		System.out.println("Sum of All Portfolio Fees :$"+DoubleFormat.format(FeesSum));
 		System.out.println();
-		System.out.println("##########################################################################################################################");
-			
+		System.out.println("##########################################################################################################################");			
 	}
 }
-//TODO: IMPLEMENT WHAT WE NEED TO WITH THE PORTFOLIO STUFF. WE MADE GOOD PROGRESS IN MAKING OUR CODE LESS SHIT
+//TODO: ADD NOTES
