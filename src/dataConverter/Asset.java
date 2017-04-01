d2e902ec09f72f7517681f31fc4d5f5d8fcb0c4e
zedinperil <zedinperil@gmail.com>
2017-03-31 19:44:45 -0500

    HEY DAWGS LOOK AT THIS DAAANK SHIT

--- src/dataConverter/Asset.java
+++ src/dataConverter/Asset.java
@@ -2,6 +2,26 @@ package dataConverter;
 
 public class Asset {
 	String portfolioCode;
+	
+	String assetCode; 
+	String assetName; 
+	String assetType; 
+	double assetValue;
+	double returnRate;
+	double annualReturn;
+	double risk;
+	double assetModifier; 
+	public Asset(String portfolio, String code, String Name, String Type, double Value, double RETURNRATE, double ANNUALRETURN, double RISK,double modifier) {
+		this.portfolioCode= portfolio;
+		this.assetCode = code;
+		this.assetName= Name;
+		this.assetType= Type;
+		this.assetValue= Value;
+		this.returnRate= RETURNRATE;
+		this.annualReturn= ANNUALRETURN;
+		this.risk= RISK;
+		this.assetModifier= modifier;
+	}	
 	/**
 	 * @return the portfolioCode
 	 */
@@ -110,24 +130,4 @@ public class Asset {
 	public void setAssetModifier(double assetModifier) {
 		this.assetModifier = assetModifier;
 	}
-	String assetCode; 
-	String assetName; 
-	String assetType; 
-	double assetValue;
-	double returnRate;
-	double annualReturn;
-	double risk;
-	double assetModifier; 
-	public Asset(String portfolio, String code, String Name, String Type, double Value, double RETURNRATE, double ANNUALRETURN, double RISK,double modifier) {
-		this.portfolioCode= portfolio;
-		this.assetCode = code;
-		this.assetName= Name;
-		this.assetType= Type;
-		this.assetValue= Value;
-		this.returnRate= RETURNRATE;
-		this.annualReturn= ANNUALRETURN;
-		this.risk= RISK;
-		this.assetModifier= modifier;
-	}	
-
 }
--- src/dataConverter/InputOutput.java
+++ src/dataConverter/InputOutput.java
@@ -14,6 +14,8 @@ import java.util.List;
 //IMPLEMENT JDBC, CONVERT TO LISTS. ESSENTIALLY THATS WHAT WE NEED TO DO.
 public class InputOutput implements databaseinfoandmethods{
 	
+	private static final double[] AnnualReturnSum = null;
+
 	public static void main(String[] args) throws IOException {			
 		//formatting for doubles
 		DecimalFormat DoubleFormat = new DecimalFormat();
@@ -41,9 +43,12 @@ public class InputOutput implements databaseinfoandmethods{
 		System.out.println("==========================================================================================================================");
 		System.out.println();
 		int g=0;
-		double TotalValueSum;
-		double FeesSum;
-		double CommissionsSum;
+		double TotalValueSum = 0;
+		double FeesSum = 0;
+		double CommissionsSum = 0;
+		double AnnualReturnSum=0;
+		double[] PortfolioAnnualReturnSum= new double[PortfolioList.size()];
+
 		while( g<PortfolioList.size()){
 			System.out.println("PORTFOLIO #"+(g+1)+"/"+PortfolioList.size());
 			System.out.println();
@@ -56,7 +61,15 @@ public class InputOutput implements databaseinfoandmethods{
 			if(!(PortfolioList.get(g).getBeneficiaryName().equals("none"))){
 				System.out.println("Beneficiary Code :"+PortfolioList.get(g).getBeneficiaryCode());
 			}
-	//		System.out.println("# Of Asset within the Portfolio :"+PortfolioList.get(g).getOccuranceOfAssetCount());
+			int NumOfAssetInPortfolio=0;
+			for(int q=0; q<AssetList.size(); q++){
+				if(AssetList.get(q).getPortfolioCode().equals(PortfolioList.get(g).getPortfolioCode())){
+					NumOfAssetInPortfolio++;
+				}
+			}
+			
+			
+			System.out.println("# Of Asset within the Portfolio :"+NumOfAssetInPortfolio);
 			System.out.println();
 			System.out.println("FINANCIAL INFORMATION");
 			System.out.println();
@@ -64,51 +77,39 @@ public class InputOutput implements databaseinfoandmethods{
 			System.out.println();
 			System.out.println("---------------------------------------------------------------------------------------------------------------------");
 			System.out.println();
-			int q=0;
-//			for(int u=0; u<PortfolioList.size(); u++){
-//					
-//					System.out.println("Asset Code :"+);
-//					q=0;
-//					while(q<PortfolioList.get(g).getAssetcount()){
-//						if(PortfolioList.get(g).getAssetName(u).equals(AssetList.get(q).getCode())){
-//							System.out.println("Asset Name :"+ AssetList.get(q).getLabel());
-//							System.out.println("Asset Type :"+ AssetList.get(q).getType());
-//							q++;
-//						}
-//						else{
-//							q++;
-//						}
-//					}
-//					System.out.println("Asset Value Modifier :"+PortfolioList.get(g).getAssetValue(u));
-//					q=0;
-//					while(q<PortfolioList.get(g).getAssetcount()){
-//						if(PortfolioList.get(g).getAssetName(u).equals(AssetList.get(q).getCode())){
-//							System.out.println("Annual Return :$"+DoubleFormat.format(PortfolioList.get(g).getAnnualReturn(u)));
-//							PortfolioAnnualReturnSum[g]+=PortfolioList.get(g).getAnnualReturn(u);
-//							AnnualReturnSum+=PortfolioList.get(g).getAnnualReturn(u);
-//							q++;
-//						}
-//						else{
-//							q++;
-//						}
-//					}
-//					System.out.println("Risk :"+DoubleFormat.format(PortfolioList.get(g).getRisk(u)));
-//					System.out.println("Value :$"+DoubleFormat.format(PortfolioList.get(g).getValue(u)));
-//					System.out.println("Return Rate :"+DoubleFormat.format((PortfolioList.get(g).getReturnRate()*100))+"%");	
-//					System.out.println();
-//					System.out.println("---------------------------------------------------------------------------------------------------------------------");
-//					System.out.println();
-//				}
+			int t=0;
+		//need to get: Asset Code, Asset Name, Asset Type, Asset Value Modifier, Annual return, risk, value, and return rate for each asset for the given portfolio Also calculate the stuff you need to
+			int a=0;
+			while(t<AssetList.size()){
+						if(AssetList.get(t).getPortfolioCode().equals(PortfolioList.get(g).getPortfolioCode())){
+							System.out.println("Asset #"+(a+1)+"/"+AssetList.size());
+
+							System.out.println("Asset Code :"+AssetList.get(t).getAssetCode());
+							System.out.println("Asset Name :"+ AssetList.get(t).getAssetName());
+							System.out.println("Asset Type :"+ AssetList.get(t).getAssetType());
+							System.out.println("Asset Value Modifier :"+AssetList.get(t).getAssetValue());				
+							System.out.println("Annual Return :$"+DoubleFormat.format(AssetList.get(t).getAnnualReturn()));
+							PortfolioAnnualReturnSum[g]+=AssetList.get(t).getAnnualReturn();
+						    AnnualReturnSum+= AssetList.get(t).getAnnualReturn();
+						    System.out.println("Risk :"+DoubleFormat.format(AssetList.get(t).getRisk()));
+						    System.out.println("Value :$"+DoubleFormat.format(AssetList.get(t).getAssetValue()));
+						    System.out.println("Return Rate :"+DoubleFormat.format((AssetList.get(t).getReturnRate()*100))+"%");	
+						    System.out.println();
+						    System.out.println("---------------------------------------------------------------------------------------------------------------------");
+						    System.out.println();  
+						    a++;
+						}
+			t++;
+			}			
 			System.out.println("SUMMATIVE FINANCIAL INFORMATION FOR PORTFOLIO "+PortfolioList.get(g).getPortfolioCode()+" #"+(g+1)+"/"+PortfolioList.size());
 			System.out.println("Total Value :$"+DoubleFormat.format(+PortfolioList.get(g).getTotalValue()));
-//			TotalValueSum+=PortfolioList.get(g).getTotalValue();
+			TotalValueSum+=PortfolioList.get(g).getTotalValue();
 			System.out.println("Aggregate Risk :"+RiskFormat.format(PortfolioList.get(g).getAggRisk()));
 			System.out.println("Total Fees :$"+DoubleFormat.format(PortfolioList.get(g).getFees()));
-//			FeesSum+=PortfolioList.get(g).getFees();
+			FeesSum+=PortfolioList.get(g).getFees();
 			System.out.println("Commissions :$"+DoubleFormat.format(PortfolioList.get(g).getCommissions()));
-//			CommissionsSum+=PortfolioList.get(g).getCommissions()/2;
-			Double[] PortfolioAnnualReturnSum;
-//			System.out.println("Portfolio Sum of Annual Returns :$"+DoubleFormat.format(PortfolioAnnualReturnSum[g]));
+			CommissionsSum+=PortfolioList.get(g).getCommissions()/2;
+			System.out.println("Portfolio Sum of Annual Returns :$"+DoubleFormat.format(PortfolioAnnualReturnSum[g]));
 			System.out.println("Value of Portfolio after Commissions and fees: $"+DoubleFormat.format(PortfolioList.get(g).getTotalValue()-PortfolioList.get(g).getFees()-PortfolioList.get(g).getCommissions()));
 			System.out.println();
 			System.out.println("END OF PORTFOLIO #"+(g+1)+"/"+PortfolioList.size());
@@ -120,11 +121,11 @@ public class InputOutput implements databaseinfoandmethods{
 		System.out.println();
 		System.out.println("SUMS OF PORTFOLIO VALUES");
 		System.out.println();
-//		System.out.println("Sum of All Portfolio Total Asset Values :$"+DoubleFormat.format(TotalValueSum));
-		Double AnnualReturnSum;
-//		System.out.println("Sum of All Portfolio Annual Returns :$"+DoubleFormat.format(AnnualReturnSum));
-//		System.out.println("Sum of All Portfolio Commissions :$"+DoubleFormat.format(CommissionsSum));
-//		System.out.println("Sum of All Portfolio Fees :$"+DoubleFormat.format(FeesSum));
+		System.out.println("Sum of All Portfolio Total Asset Values :$"+DoubleFormat.format(TotalValueSum));
+		
+		System.out.println("Sum of All Portfolio Annual Returns :$"+DoubleFormat.format(AnnualReturnSum));
+		System.out.println("Sum of All Portfolio Commissions :$"+DoubleFormat.format(CommissionsSum));
+		System.out.println("Sum of All Portfolio Fees :$"+DoubleFormat.format(FeesSum));
 		System.out.println();
 		System.out.println("##########################################################################################################################");
 			
