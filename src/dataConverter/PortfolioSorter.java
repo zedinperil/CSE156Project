699d196a30c402af7fe7b606e0817da49a0287bf
zedinperil <zedinperil@gmail.com>
2017-04-21 18:06:45 -0500

    6.2.insertionsort

--- src/dataConverter/PortfolioSorter.java
+++ src/dataConverter/PortfolioSorter.java
@@ -1,7 +1,21 @@
 package dataConverter;
 
-public interface PortfolioSorter {
+public class PortfolioSorter {
 
+
+	public static void insertionSort (List<Portfolio> portfolioArray()) {
+		 Portfolio tempPort;
+		 for (int i =1; i < portfolioArray.size() ; i ++) {
+			 tempPort = portfolioArray.get(i);
+		 	int j = i ;
+		//fix this comparison to change how it is sorted
+		 	while ( j > 0 && portfolioArray.get(j-1) > tempPort) {
+		 	portfolioArray.get(j) = portfolioArray.get(j-1);
+		 	j--;
+		 	}
+		 	portfolioArray.get(j) = tempPort ;
+		 }
+	}
 	
 
 }
